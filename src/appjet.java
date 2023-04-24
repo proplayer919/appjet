0fb04123a771d9b816301d827165121739dbe4d6
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-04-25 09:14:49 +1000

    update

---
+++ src/appjet.java
@@ -0,0 +1,22 @@
+package org.fifly.appjet;
+
+import javax.swing.*;
+
+class Jet extends JFrame{
+    public Jet(String title) {
+        super(title);
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void show() {
+        this.setVisible(true);
+    }
+}
+
+class DefaultHandle {
+    public DefaultHandle() {
+        Jet jet = new Jet("An Appjet app");
+        jet.show();
+    }
+}
